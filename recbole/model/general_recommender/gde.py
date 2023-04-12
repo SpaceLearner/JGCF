cab48b8a5202371761bfc44e8ea9a7264d37f67f
SpaceLearner <Guojy001@gmail.com>
2023-04-12 09:50:27 +0800

    commit

--- recbole/model/general_recommender/gde.py
+++ recbole/model/general_recommender/gde.py
@@ -77,14 +77,24 @@ class GDE(GeneralRecommender):
             
         elif config["feature_type"]=='both':
 
-            user_value, user_vec     = cal_spectral_feature(Adj=self.Adj.matmul(self.Adj.T).clip(max=1.0), size=int(self.n_users*self.smooth_ratio))
+            user_adj  = self.Adj.matmul(self.Adj.T).clip(max=1.0)
+            user_adj_index = user_adj.nonzero().T
+            adj_value = user_adj[user_adj_index[0], user_adj_index[1]]
+            user_adj = torch.sparse_coo_tensor(indices=user_adj_index.cpu(), values=adj_value.cpu())
+            user_value, user_vec     = cal_spectral_feature(Adj=user_adj, size=int(self.n_users*self.smooth_ratio))
             user_value_r, user_vec_r = cal_spectral_feature(Adj=self.Adj.matmul(self.Adj.T).clip(max=1.0), size=int(self.n_users*self.rough_ratio), largest=False)
             
             user_filter=torch.cat([self.weight_feature(user_value.to(self.device)), self.weight_feature(user_value_r.to(self.device))])
             user_vector=torch.cat([user_vec.to(self.device), user_vec_r.to(self.device)],1)
             
-            item_value, item_vec     = cal_spectral_feature(Adj=self.Adj.T.matmul(self.Adj).clip(max=1.0), size=int(self.n_users*self.smooth_ratio))
-            item_value_r, item_vec_r = cal_spectral_feature(Adj=self.Adj.T.matmul(self.Adj).clip(max=1.0), size=int(self.n_users*self.rough_ratio), largest=False)
+            
+            item_adj  = self.Adj.T.matmul(self.Adj).clip(max=1.0)
+            item_adj_index = item_adj.nonzero().T
+            adj_value = item_adj[item_adj_index[0], item_adj_index[1]]
+            
+            item_adj = torch.sparse_coo_tensor(indices=item_adj_index.cpu(), values=adj_value.cpu())
+            item_value, item_vec     = cal_spectral_feature(Adj=item_adj, size=int(self.n_users*self.smooth_ratio))
+            item_value_r, item_vec_r = cal_spectral_feature(Adj=item_adj, size=int(self.n_users*self.rough_ratio), largest=False)
             
             item_filter=torch.cat([self.weight_feature(item_value.to(self.device)),self.weight_feature(item_value_r.to(self.device))])
             item_vector=torch.cat([item_vec, item_vec_r], dim=1)
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from recbole.quick_start import run_recbole
 
 # run_recbole(model="LightGCN_Poly", dataset="TenRec", config_file_list=["config_tenrec.yaml"])
 
-run_recbole(model="JGCF", dataset="ml_1m", config_file_list=["configs/config_ml_1m.yaml"])
+run_recbole(model="LightGCN", dataset="ml_1m", config_file_list=["configs/config_ml_1m.yaml"])
